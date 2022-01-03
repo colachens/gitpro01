383911f8e6e3232f5dc1098683e32bbe76d402b7
colachens <252200969@qq.com>
2022-01-03 11:42:50 +0800

    用户1号先提交

--- src/main/java/com/senchen/App.java
+++ src/main/java/com/senchen/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "**User01 ： 01" );
+        System.out.println( "**User01 ： 02" );
+        System.out.println( "**Hello World!" );
     }
 }
